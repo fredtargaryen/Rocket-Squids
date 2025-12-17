db4b993957f6f92dd49a983a7cd5fba3dfebbc87
barnabeepickle <picklebarnabee@gmail.com>
2025-12-17 11:27:01 -0500

    changed Conchzilla's name so that "Blessed Conch of the Squidseeker" is now lore text and fixed missing enchantent glint

--- src/main/java/com/fredtargaryen/rocketsquids/item/ItemConch3.java
+++ src/main/java/com/fredtargaryen/rocketsquids/item/ItemConch3.java
@@ -1,15 +1,21 @@
 package com.fredtargaryen.rocketsquids.item;
 
 import com.fredtargaryen.rocketsquids.RocketSquidsBase;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemConch3 extends Item {
     public ItemConch3(Item.Properties properties) {
@@ -25,6 +31,13 @@ public class ItemConch3 extends Item {
         return new InteractionResultHolder<>(InteractionResult.PASS, playerIn.getItemInHand(handIn));
     }
 
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltipComponents, TooltipFlag isAdvanced) {
+        super.appendHoverText(stack, level, tooltipComponents, isAdvanced);
+        tooltipComponents.add(new TranslatableComponent("item.rocketsquids.conch_item_3.lore"));
+    }
+
     /**
      * Returns true if this item has an enchantment glint. By default, this returns
      * <code>stack.isItemEnchanted()</code>, but other items can override it (for instance, written books always return
@@ -34,7 +47,7 @@ public class ItemConch3 extends Item {
      * the glint for enchanted items. Of course, that is unnecessary if the overwritten version always returns true.
      */
     @OnlyIn(Dist.CLIENT)
-    public boolean hasEffect(ItemStack stack)
+    public boolean isFoil(ItemStack stack)
     {
         return true;
     }
