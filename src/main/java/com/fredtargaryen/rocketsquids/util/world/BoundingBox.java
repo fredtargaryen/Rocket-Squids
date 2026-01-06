b36c2d924654b3be09be081f44d89437e783769f
barnabeepickle <picklebarnabee@gmail.com>
2026-01-06 13:01:23 -0500

    created new utilities for AABB bounding boxes

---
+++ src/main/java/com/fredtargaryen/rocketsquids/util/world/BoundingBox.java
@@ -0,0 +1,56 @@
+package com.fredtargaryen.rocketsquids.util.world;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+
+import java.awt.*;
+
+public class BoundingBox {
+    /** Creates an AABB boundbox of the spesified radius
+     * @param center BlockPos for the center of the bounding box
+     * @param radiusSize double for the radius or size of the bounding box
+     * @return returns an AABB bounding box
+     */
+    public AABB cubeFromPosition(BlockPos center, double radiusSize) {
+        radiusSize = Math.abs(radiusSize);
+        BlockPos start = center.offset(radiusSize, radiusSize, radiusSize);
+        BlockPos end = center.offset(-radiusSize, -radiusSize, -radiusSize);
+        return new AABB(start, end);
+    }
+
+    /** Creates an AABB boundbox of the spesified radius then clamps it to the minHeight & maxHeight respectively.
+     * @param center BlockPos for the center of the bounding box
+     * @param radiusSize double for the radius or size of the bounding box
+     * @param maxHeight maximum Y level to clamp to
+     * @param minHeight minimum Y level to clamp to
+     * @return returns an AABB bounding box
+     */
+    public AABB cubeFromPositionClamped(BlockPos center, double radiusSize, int maxHeight, int minHeight) {
+        radiusSize = Math.abs(radiusSize);
+        BlockPos start = center.offset(radiusSize, Math.max(radiusSize, maxHeight), radiusSize);
+        BlockPos end = center.offset(-radiusSize, -Math.min(radiusSize, minHeight), -radiusSize);
+        return new AABB(start, end);
+    }
+
+    /** Creates an AABB boundbox of the spesified radius then clamps it to the height limit in box directions.
+     * @param center BlockPos for the center of the bounding box
+     * @param radiusSize double for the radius or size of the bounding box
+     * @param level Level to get the min & max build heights from
+     * @return returns an AABB bounding box
+     */
+    public AABB cubeFromPositionClamped(BlockPos center, double radiusSize, Level level) {
+        return cubeFromPositionClamped(center, radiusSize, level.getMaxBuildHeight(), level.getMinBuildHeight());
+    }
+
+    /** Creates an AABB boundbox of the spesified radius then clamps it to the height limit in box directions.
+     * @param center BlockPos for the center of the bounding box
+     * @param radiusSize double for the radius or size of the bounding box
+     * @param level ServerLevel to get the min & max build heights from
+     * @return returns an AABB bounding box
+     */
+    public AABB cubeFromPositionClamped(BlockPos center, double radiusSize, ServerLevel level) {
+        return cubeFromPositionClamped(center, radiusSize, level.getMaxBuildHeight(), level.getMinBuildHeight());
+    }
+}
