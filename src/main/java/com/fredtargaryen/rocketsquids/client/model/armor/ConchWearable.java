37848a1ec89cae99cb2aed2e7ef5a5278b87b51b
barnabeepickle <picklebarnabee@gmail.com>
2026-01-06 13:37:15 -0500

    added ResourceLocations to ConchWearable

--- src/main/java/com/fredtargaryen/rocketsquids/client/model/armor/ConchWearable.java
+++ src/main/java/com/fredtargaryen/rocketsquids/client/model/armor/ConchWearable.java
@@ -1,4 +1,24 @@
 package com.fredtargaryen.rocketsquids.client.model.armor;
 
-public class ConchWearable {
+import com.fredtargaryen.rocketsquids.item.GeoModArmorItem;
+import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+
+import static com.fredtargaryen.rocketsquids.DataReference.MODID;
+
+public class ConchWearable extends AnimatedGeoModel<GeoModArmorItem> {
+    @Override
+    public ResourceLocation getModelLocation(GeoModArmorItem object) {
+        return new ResourceLocation(MODID, ":geo/armor/conch_armor_model.geo.json");
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(GeoModArmorItem object) {
+        return new ResourceLocation(MODID, ":textures/models/armor/conch_armor_model.png");
+    }
+
+    @Override
+    public ResourceLocation getAnimationFileLocation(GeoModArmorItem animatable) {
+        return new ResourceLocation(MODID, ":animations/armor/armor.animation.json");
+    }
 }
