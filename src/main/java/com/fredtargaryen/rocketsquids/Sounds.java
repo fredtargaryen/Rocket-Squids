2e121ddef118bf1738d0fce7ab2e12f2ea872f17
barnabeepickle <picklebarnabee@gmail.com>
2026-01-16 13:39:01 -0500

    started fixing Sounds.java

--- src/main/java/com/fredtargaryen/rocketsquids/Sounds.java
+++ src/main/java/com/fredtargaryen/rocketsquids/Sounds.java
@@ -2,10 +2,24 @@ package com.fredtargaryen.rocketsquids;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.RegistryObject;
+
+import static com.fredtargaryen.rocketsquids.DataReference.MODID;
 
 public class Sounds {
+    public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
+            DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, MODID);
+
+    public static final RegistryObject<SoundEvent> BLASTOFF = registerSoundEvents("blastoff");
+
+
+    private static RegistryObject<SoundEvent> registerSoundEvents(String name) {
+        return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, name)));
+    }
+
     ////////////////////
     ///Declare sounds///
     ////////////////////
@@ -15,85 +29,45 @@ public class Sounds {
     public static SoundEvent[] CONCH_NOTES;
 
     public static void constructAndRegisterSoundEvents(RegistryEvent.Register<SoundEvent> event) {
-        BLASTOFF = new SoundEvent(new ResourceLocation(DataReference.MODID, "blastoff"))
-                .setRegistryName("blastoff");
-        SQUIDTP_IN = new SoundEvent(new ResourceLocation(DataReference.MODID, "tpin"))
-                .setRegistryName("tpin");
-        SQUIDTP_OUT = new SoundEvent(new ResourceLocation(DataReference.MODID, "tpout"))
-                .setRegistryName("tpout");
-        CONCH_NOTES = new SoundEvent[]{
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchc3"))
-                        .setRegistryName("conchc3"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchcs3"))
-                        .setRegistryName("conchcs3"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchd3"))
-                        .setRegistryName("conchd3"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchds3"))
-                        .setRegistryName("conchds3"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conche3"))
-                        .setRegistryName("conche3"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchf3"))
-                        .setRegistryName("conchf3"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchfs3"))
-                        .setRegistryName("conchfs3"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchg3"))
-                        .setRegistryName("conchg3"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchgs3"))
-                        .setRegistryName("conchgs3"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "concha3"))
-                        .setRegistryName("concha3"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchas3"))
-                        .setRegistryName("conchas3"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchb3"))
-                        .setRegistryName("conchb3"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchc4"))
-                        .setRegistryName("conchc4"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchcs4"))
-                        .setRegistryName("conchcs4"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchd4"))
-                        .setRegistryName("conchd4"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchds4"))
-                        .setRegistryName("conchds4"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conche4"))
-                        .setRegistryName("conche4"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchf4"))
-                        .setRegistryName("conchf4"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchfs4"))
-                        .setRegistryName("conchfs4"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchg4"))
-                        .setRegistryName("conchg4"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchgs4"))
-                        .setRegistryName("conchgs4"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "concha4"))
-                        .setRegistryName("concha4"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchas4"))
-                        .setRegistryName("conchas4"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchb4"))
-                        .setRegistryName("conchb4"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchc5"))
-                        .setRegistryName("conchc5"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchcs5"))
-                        .setRegistryName("conchcs5"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchd5"))
-                        .setRegistryName("conchd5"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchds5"))
-                        .setRegistryName("conchds5"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conche5"))
-                        .setRegistryName("conche5"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchf5"))
-                        .setRegistryName("conchf5"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchfs5"))
-                        .setRegistryName("conchfs5"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchg5"))
-                        .setRegistryName("conchg5"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchgs5"))
-                        .setRegistryName("conchgs5"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "concha5"))
-                        .setRegistryName("concha5"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchas5"))
-                        .setRegistryName("conchas5"),
-                new SoundEvent(new ResourceLocation(DataReference.MODID, "conchb5"))
-                        .setRegistryName("conchb5")
+        SQUIDTP_IN = SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "tpin"));
+        SQUIDTP_OUT = SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "tpout"));
+        CONCH_NOTES = new SoundEvent[] {
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchc3")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchcs3")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchd3")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchds3")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conche3")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchf3")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchfs3")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchg3")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchgs3")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "concha3")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchas3")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchb3")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchc4")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchcs4")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchd4")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchds4")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conche4")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchf4")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchfs4")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchg4")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchgs4")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "concha4")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchas4")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchb4")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchc5")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchcs5")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchd5")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchds5")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conche5")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchf5")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchfs5")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchg5")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchgs5")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "concha5")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchas5")),
+                SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, "conchb5"))
         };
         IForgeRegistry<SoundEvent> reg = event.getRegistry();
         reg.registerAll(BLASTOFF, SQUIDTP_IN, SQUIDTP_OUT);
