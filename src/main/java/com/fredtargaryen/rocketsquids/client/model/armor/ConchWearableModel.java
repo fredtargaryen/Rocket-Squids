c0d6a8b428d4f69a01b21a7579c79ae49434eb74
barnabeepickle <picklebarnabee@gmail.com>
2026-01-16 13:08:04 -0500

    fixed crash due to and accidental un-needed set of :

--- src/main/java/com/fredtargaryen/rocketsquids/client/model/armor/ConchWearableModel.java
+++ src/main/java/com/fredtargaryen/rocketsquids/client/model/armor/ConchWearableModel.java
@@ -9,16 +9,16 @@ import static com.fredtargaryen.rocketsquids.DataReference.MODID;
 public class ConchWearableModel extends AnimatedGeoModel<ItemConch> {
     @Override
     public ResourceLocation getModelLocation(ItemConch object) {
-        return new ResourceLocation(MODID, ":geo/armor/conch_armor_model.geo.json");
+        return new ResourceLocation(MODID, "geo/armor/conch_armor_model.geo.json");
     }
 
     @Override
     public ResourceLocation getTextureLocation(ItemConch object) {
-        return new ResourceLocation(MODID, ":textures/models/armor/conch_armor_model.png");
+        return new ResourceLocation(MODID, "textures/models/armor/conch_armor_model.png");
     }
 
     @Override
     public ResourceLocation getAnimationFileLocation(ItemConch animatable) {
-        return new ResourceLocation(MODID, ":animations/armor/armor.animation.json");
+        return new ResourceLocation(MODID, "animations/armor/armor.animation.json");
     }
 }
