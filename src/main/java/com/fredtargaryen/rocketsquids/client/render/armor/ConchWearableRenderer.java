9d2cc343973b0c19ad70e813553627b7b5ca8978
barnabeepickle <picklebarnabee@gmail.com>
2026-01-16 13:15:42 -0500

    fixed incorrect bone names in ConchWearableRenderer

--- src/main/java/com/fredtargaryen/rocketsquids/client/render/armor/ConchWearableRenderer.java
+++ src/main/java/com/fredtargaryen/rocketsquids/client/render/armor/ConchWearableRenderer.java
@@ -9,13 +9,13 @@ public class ConchWearableRenderer extends GeoArmorRenderer<ItemConch> {
         super(new ConchWearableModel());
         // These values are what each bone name is in blockbench.
         // The default values are the ones that come with the default armor template in the GeckoLib Blockbench plugin.
-        this.headBone = "helmet";
-        this.bodyBone = "chestplate";
-        this.rightArmBone = "rightArm";
-        this.leftArmBone = "leftArm";
-        this.rightLegBone = "rightLeg";
-        this.leftLegBone = "leftLeg";
-        this.rightBootBone = "rightBoot";
-        this.leftBootBone = "leftBoot";
+        this.headBone = "armorHead";
+        this.bodyBone = "armorBody";
+        this.rightArmBone = "armorRightArm";
+        this.leftArmBone = "armorLeftArm";
+        this.leftLegBone = "armorLeftLeg";
+        this.leftBootBone = "armorLeftBoot";
+        this.rightLegBone = "armorRightLeg";
+        this.rightBootBone = "armorRightBoot";
     }
 }
