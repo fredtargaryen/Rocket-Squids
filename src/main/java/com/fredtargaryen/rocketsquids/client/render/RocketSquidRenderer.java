0dcd509e5292d8ff96ddd31bb4753905da4940ad
fredtargaryen <lggy@hotmail.co.uk>
2026-04-25 17:23:37 +0100

    Fix fire not displaying when a rocket squid is blasting through the air

--- src/main/java/com/fredtargaryen/rocketsquids/client/render/RocketSquidRenderer.java
+++ src/main/java/com/fredtargaryen/rocketsquids/client/render/RocketSquidRenderer.java
@@ -72,7 +72,9 @@ public class RocketSquidRenderer extends MobRenderer<RocketSquidEntity, RocketSq
         if (par1EntitySquid.getShaking()) {
             RandomSource r = par1EntitySquid.getRandom();
             matrixStackIn.translate(r.nextGaussian() * 0.02d, r.nextGaussian() * 0.02d, r.nextGaussian() * 0.02d);
-        } else if (par1EntitySquid.getBlasting() && !par1EntitySquid.isInWater()) {
+        }
+
+        if (par1EntitySquid.getBlasting() && !par1EntitySquid.isInWater()) {
             //Choose texture
             TextureAtlasSprite tas = ModelBakery.FIRE_0.sprite();
             //Calculate and set translation-rotation matrix
