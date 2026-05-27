933155c633b9182f83b65332a97ae54e656e6c72
fredtargaryen <lggy@hotmail.co.uk>
2026-05-28 00:37:50 +0100

    Fix Squeleporter not releasing rocket squids

--- src/main/java/com/fredtargaryen/rocketsquids/util/ValueIOHelper.java
+++ src/main/java/com/fredtargaryen/rocketsquids/util/ValueIOHelper.java
@@ -2,23 +2,25 @@
 // See README.md for full copyright notice and contributor info
 package com.fredtargaryen.rocketsquids.util;
 
+import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.storage.TagValueInput;
 import net.minecraft.world.level.storage.TagValueOutput;
 import net.minecraft.world.level.storage.ValueInput;
 import net.neoforged.neoforge.common.util.ValueIOSerializable;
 
+import java.util.stream.Stream;
+
 public class ValueIOHelper {
+    private static final HolderLookup.Provider dummyHoldersProvider = HolderLookup.Provider.create(Stream.empty());
+
     public static CompoundTag getValueIOAsCompoundTag(ValueIOSerializable data) {
         TagValueOutput vo = TagValueOutput.createWithoutContext(null);
         data.serialize(vo);
         return vo.buildResult();
     }
 
-    /**
-     * TODO Likely to throw its toys out of the pram if the provder is null - can I have a dummy provider to keep it happy?
-     */
     public static ValueInput getCompoundTagAsValueInput(CompoundTag tag) {
-        return TagValueInput.create(null, null, tag);
+        return TagValueInput.create(null, dummyHoldersProvider, tag);
     }
 }
