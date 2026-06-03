cd601fcad5ad87de7f2744f1074d773861ed9d4d
fredtargaryen <lggy@hotmail.co.uk>
2026-06-03 12:33:56 +0100

    SwimAroundGoal: remove StatueBlastStage

--- src/main/java/com/fredtargaryen/rocketsquids/level/entity/ai/SwimAroundGoal.java
+++ src/main/java/com/fredtargaryen/rocketsquids/level/entity/ai/SwimAroundGoal.java
@@ -22,22 +22,13 @@ public class SwimAroundGoal extends Goal {
     private final RocketSquidEntity squid;
     private int tickCounter;
     private int nextScheduledMove;
-
-    private enum StatueBlastStage {
-        NONE,
-        LOCATE,
-        TURN
-    }
-
     private final RandomSource r;
-    private StatueBlastStage statueBlastStage;
 
     public SwimAroundGoal(RocketSquidEntity ers) {
         super();
         this.squid = ers;
         this.setFlags(EnumSet.of(Flag.MOVE));
         this.r = this.squid.getRandom();
-        this.statueBlastStage = StatueBlastStage.NONE;
         this.tickCounter = 0;
         this.nextScheduledMove = 0;
     }
@@ -107,65 +98,51 @@ public class SwimAroundGoal extends Goal {
         double ry = this.squid.getYaw();
         if (!this.squid.isBaby() && this.squid.blastingToStatue) {
             //Override all behaviour if it heard its target notes and needs to find and blast to a statue
-            switch (this.statueBlastStage) {
-                case NONE:
-                    this.statueBlastStage = StatueBlastStage.LOCATE;
-                    break;
-                case LOCATE:
-                    //Find nearest statue
-                    Vec3 pos = this.squid.position();
-                    List<Integer> statueCoords = StatueData.forLevel(this.squid.level()).getNearestStatuePos(pos.x, pos.y, pos.z);
-                    if (statueCoords == null) {
-                        //StatueManager doesn't have any statues loaded
-                        this.statueBlastStage = StatueBlastStage.NONE;
-                        this.squid.blastingToStatue = false;
-                    } else {
-                        double zDistance = statueCoords.get(4) - pos.z;
-                        double xDistance = statueCoords.get(2) - pos.x;
-                        double hozDistanceSquared = zDistance * zDistance + xDistance * xDistance;
-                        //Turn in direction of nearest statue. Not sure why but these values are necessary for it to point correctly
-                        this.squid.setTargetYaw(Math.atan2(-xDistance, zDistance));
-                        // Send "Recognition" empty sound for those using subs
-                        MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 36), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
-                        //Play a celebratory chord
-                        if (hozDistanceSquared > 640000.0) {
-                            //More than 50 chunks away (50 * 16 = 800 blocks). Low C Major
-                            MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 0), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
-                            MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 4), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
-                            MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 7), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
-                        } else if (hozDistanceSquared > 25600.0) {
-                            //10-50 chunks away (10 * 16 = 160 blocks). Middle C Major
-                            MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 12), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
-                            MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 16), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
-                            MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 19), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
-                        } else {
-                            //Less than 10 chunks away. High C Major
-                            MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 24), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
-                            MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 28), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
-                            MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 31), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
-                        }
-                        if (hozDistanceSquared > 6400.0) {
-                            //More than 80 blocks (5 chunks) away horizontally; blast at 45 degrees so the player can hopefully see easily
-                            //A squid can go about 80 blocks at surface level and 45 degrees so this should prevent some annoying overshooting
-                            this.squid.setTargetPitch(Math.PI / 4.0);
-                        } else {
-                            //Less than 80 blocks away; blast directly towards the statue
-                            this.squid.setTargetPitch(Math.atan2(pos.y - statueCoords.get(3), Math.sqrt(hozDistanceSquared)) + Math.PI / 2.0);
-                        }
-                        this.statueBlastStage = StatueBlastStage.TURN;
-                    }
-                    break;
-                case TURN:
-                    double trp = this.squid.getTargetPitch();
-                    double Try = this.squid.getTargetYaw();
-                    if (Math.abs(trp - rp) < 0.0005 && Math.abs(Try - ry) < 0.0005) {
-                        this.squid.setShaking(true);
-                        this.statueBlastStage = StatueBlastStage.NONE;
-                        this.squid.blastingToStatue = false;
-                    }
-                    break;
-                default:
-                    break;
+            //Find nearest statue
+            Vec3 pos = this.squid.position();
+            List<Integer> statueCoords = StatueData.forLevel(this.squid.level()).getNearestStatuePos(pos.x, pos.y, pos.z);
+            if (statueCoords == null) {
+                //StatueManager doesn't have any statues loaded
+                this.squid.blastingToStatue = false;
+            } else {
+                double zDistance = statueCoords.get(4) - pos.z;
+                double xDistance = statueCoords.get(2) - pos.x;
+                double hozDistanceSquared = zDistance * zDistance + xDistance * xDistance;
+                //Turn in direction of nearest statue. Not sure why but these values are necessary for it to point correctly
+                this.squid.setTargetYaw(Math.atan2(-xDistance, zDistance));
+                // Send "Recognition" empty sound for those using subs
+                MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 36), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
+                //Play a celebratory chord
+                if (hozDistanceSquared > 640000.0) {
+                    //More than 50 chunks away (50 * 16 = 800 blocks). Low C Major
+                    MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 0), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
+                    MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 4), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
+                    MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 7), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
+                } else if (hozDistanceSquared > 25600.0) {
+                    //10-50 chunks away (10 * 16 = 160 blocks). Middle C Major
+                    MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 12), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
+                    MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 16), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
+                    MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 19), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
+                } else {
+                    //Less than 10 chunks away. High C Major
+                    MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 24), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
+                    MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 28), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
+                    MessageHandler.sendToPlayersNear((ServerLevel) this.squid.level(), new SquidNoteMessage((byte) 31), pos.x, pos.y, pos.z, DataReference.SQUID_SING_RANGE);
+                }
+                if (hozDistanceSquared > 6400.0) {
+                    //More than 80 blocks (5 chunks) away horizontally; blast at 45 degrees so the player can hopefully see easily
+                    //A squid can go about 80 blocks at surface level and 45 degrees so this should prevent some annoying overshooting
+                    this.squid.setTargetPitch(Math.PI / 4.0);
+                } else {
+                    //Less than 80 blocks away; blast directly towards the statue
+                    this.squid.setTargetPitch(Math.atan2(pos.y - statueCoords.get(3), Math.sqrt(hozDistanceSquared)) + Math.PI / 2.0);
+                }
+            }
+            double trp = this.squid.getTargetPitch();
+            double Try = this.squid.getTargetYaw();
+            if (Math.abs(trp - rp) < 0.0005 && Math.abs(Try - ry) < 0.0005) {
+                this.squid.setShaking(true);
+                this.squid.blastingToStatue = false;
             }
         } else {
             if (this.tickCounter > this.nextScheduledMove) this.scheduleNextMove();
