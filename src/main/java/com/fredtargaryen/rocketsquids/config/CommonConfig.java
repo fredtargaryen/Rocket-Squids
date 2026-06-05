d9957bebd899cb04f1d2da99a7c7a39faa0b0f1e
fredtargaryen <lggy@hotmail.co.uk>
2026-06-05 08:29:40 +0100

    Remove some config variables that have been made obsolete by biome modifiers

--- src/main/java/com/fredtargaryen/rocketsquids/config/CommonConfig.java
+++ src/main/java/com/fredtargaryen/rocketsquids/config/CommonConfig.java
@@ -13,9 +13,6 @@ import java.util.List;
 
 @EventBusSubscriber(modid = DataReference.MODID)
 public class CommonConfig {
-    public static List<String> DEFAULT_BLACKLIST = Collections.emptyList();
-    public static List<String> DEFAULT_WHITELIST = Collections.singletonList("overworld");
-
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
     private static final ModConfigSpec.IntValue _BREED_COOLDOWN = BUILDER
@@ -28,36 +25,6 @@ public class CommonConfig {
             .translation("config.common.squid.explosion.destructive")
             .define("squid.explosion.destructive", true);
 
-    private static final ModConfigSpec.BooleanValue _CONCH_USE_WHITELIST = BUILDER
-            .comment("If true, uses the conch whitelist to determine where conches can be found. If false, uses the blacklist.")
-            .translation("config.common.worldgen.conch.usewhitelist")
-            .define("worldgen.conch.usewhitelist", true);
-
-    private static final ModConfigSpec.ConfigValue<List<? extends String>> _CONCH_WHITELIST = BUILDER
-            .comment("The list of dimensions where conches can appear.")
-            .translation("config.common.worldgen.conch.whitelist")
-            .defineList("worldgen.conch.whitelist", DEFAULT_WHITELIST, string -> true);
-
-    private static final ModConfigSpec.ConfigValue<List<? extends String>> _CONCH_BLACKLIST = BUILDER
-            .comment("The list of dimensions where conches cannot appear.")
-            .translation("config.common.worldgen.conch.blacklist")
-            .defineList("worldgen.conch.blacklist", DEFAULT_BLACKLIST, string -> true);
-
-    private static final ModConfigSpec.BooleanValue _STATUE_USE_WHITELIST = BUILDER
-            .comment("If true, uses the statue whitelist to determine where statues can be found. If false, uses the blacklist.")
-            .translation("config.common.worldgen.statue.usewhitelist")
-            .define("worldgen.statue.usewhitelist", true);
-
-    private static final ModConfigSpec.ConfigValue<List<? extends String>> _STATUE_WHITELIST = BUILDER
-            .comment("The list of dimensions where statues can appear.")
-            .translation("config.common.worldgen.statue.whitelist")
-            .defineList("worldgen.statue.whitelist", DEFAULT_WHITELIST, string -> true);
-
-    private static final ModConfigSpec.ConfigValue<List<? extends String>> _STATUE_BLACKLIST = BUILDER
-            .comment("The list of dimensions where statues cannot appear.")
-            .translation("config.common.worldgen.statue.blacklist")
-            .defineList("worldgen.statue.blacklist", DEFAULT_BLACKLIST, string -> true);
-
     private static final ModConfigSpec.IntValue _STATUE_FREQUENCY = BUILDER
             .comment("One statue will appear in every nxn chunk area. Changing this in an existing world is not recommended.")
             .translation("config.common.worldgen.statue.frequency")
@@ -65,30 +32,14 @@ public class CommonConfig {
 
     public static final ModConfigSpec SPEC = BUILDER.build();
 
-    public static boolean CONCH_USE_WHITELIST;
-    public static List<? extends String> CONCH_WHITELIST;
-    public static List<? extends String> CONCH_BLACKLIST;
-
     public static int BREED_COOLDOWN;
     public static boolean ROCKET_SQUID_EXPLOSIONS_DESTROY;
-
-    public static boolean STATUE_USE_WHITELIST;
-    public static List<? extends String> STATUE_WHITELIST;
-    public static List<? extends String> STATUE_BLACKLIST;
     public static int STATUE_FREQUENCY;
 
     @SubscribeEvent
     static void commonSetup(final FMLCommonSetupEvent event) {
-        CONCH_USE_WHITELIST = _CONCH_USE_WHITELIST.get();
-        CONCH_WHITELIST = _CONCH_WHITELIST.get();
-        CONCH_BLACKLIST = _CONCH_BLACKLIST.get();
-
         BREED_COOLDOWN = _BREED_COOLDOWN.get();
         ROCKET_SQUID_EXPLOSIONS_DESTROY = _ROCKET_SQUID_EXPLOSIONS_DESTROY.get();
-
-        STATUE_USE_WHITELIST = _STATUE_USE_WHITELIST.get();
-        STATUE_WHITELIST = _STATUE_WHITELIST.get();
-        STATUE_BLACKLIST = _STATUE_BLACKLIST.get();
         STATUE_FREQUENCY = _STATUE_FREQUENCY.get();
     }
 }
