c9fca8866dc9c829075cc4b2d37f8584820e3250
fredtargaryen <lggy@hotmail.co.uk>
2026-06-05 08:40:33 +0100

    Allow whistling conches, nitro ink sacs and turbo tubes to stack to 64

--- src/main/java/com/fredtargaryen/rocketsquids/RSItems.java
+++ src/main/java/com/fredtargaryen/rocketsquids/RSItems.java
@@ -17,13 +17,13 @@ public class RSItems {
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(BuiltInRegistries.ITEM, MODID);
 
     // Register all items here
-    public static final DeferredHolder<Item, ItemConch> ITEM_CONCH = ITEMS.register("conch_item_1", () -> new ItemConch(new Item.Properties().stacksTo(4)));
+    public static final DeferredHolder<Item, ItemConch> ITEM_CONCH = ITEMS.register("conch_item_1", () -> new ItemConch(new Item.Properties().stacksTo(64)));
     @SuppressWarnings("unused")
     public static final DeferredHolder<Item, ItemConch2> ITEM_CONCH2 = ITEMS.register("conch_item_2", () -> new ItemConch2(new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON)));
     @SuppressWarnings("unused")
     public static final DeferredHolder<Item, ItemConch3> ITEM_CONCH3 = ITEMS.register("conch_item_3", () -> new ItemConch3(new Item.Properties().stacksTo(1).rarity(Rarity.RARE)));
-    public static final DeferredHolder<Item, ItemNitroInkSac> NITRO_SAC = ITEMS.register("nitro_ink_sac", () -> new ItemNitroInkSac(new Item.Properties().stacksTo(16)));
-    public static final DeferredHolder<Item, ItemTurboTube> TURBO_TUBE = ITEMS.register("turbo_tube", () -> new ItemTurboTube(new Item.Properties().stacksTo(16).rarity(Rarity.UNCOMMON)));
+    public static final DeferredHolder<Item, ItemNitroInkSac> NITRO_SAC = ITEMS.register("nitro_ink_sac", () -> new ItemNitroInkSac(new Item.Properties().stacksTo(64)));
+    public static final DeferredHolder<Item, ItemTurboTube> TURBO_TUBE = ITEMS.register("turbo_tube", () -> new ItemTurboTube(new Item.Properties().stacksTo(64).rarity(Rarity.UNCOMMON)));
     public static final DeferredHolder<Item, BlockItem> ITEM_STATUE_CLOSED = ITEMS.register("statue_closed", () -> new BlockItem(RSBlocks.STATUE.get(), new Item.Properties().stacksTo(1).rarity(Rarity.RARE)));
     public static final DeferredHolder<Item, BlockItem> ITEM_STATUE_OPEN = ITEMS.register("statue_open", () -> new BlockItem(RSBlocks.STATUE.get(), new Item.Properties().stacksTo(1)));
     public static final DeferredHolder<Item, ItemSqueleporter> SQUELEPORTER_ACTIVE = ITEMS.register("squeleporter_active", () -> new ItemSqueleporter(new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON)));
